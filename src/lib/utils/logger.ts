8b99d309c7cb869d173767ffc3abb733c70d4937
Swashata Ghosh <swashata@wpquark.com>
2021-09-01 09:38:49 +0530

    chore: initial commit

---
+++ src/lib/utils/logger.ts
@@ -0,0 +1,17 @@
+export const Logger = {
+	Log(...args: any[]) {
+		if (console && console.log) {
+			console.log(...args);
+		}
+	},
+	Error(...args: any[]) {
+		if (console && console.error) {
+			console.error(...args);
+		}
+	},
+	Warn(...args: any[]) {
+		if (console && console.warn) {
+			console.warn(...args);
+		}
+	},
+};
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import Checkout from './lib/checkout';
+
+import './style.css';
+
+Checkout();
