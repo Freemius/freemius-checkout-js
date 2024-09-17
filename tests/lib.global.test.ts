fa4039b57c79cf5f862074aacd4c05e51a904e27
Swashata Ghosh <swas@freemius.com>
2024-09-17 20:41:55 +0530

    [build] [sourcemap] Add external sourcemap for easier production debugging.

--- tests/lib.global.test.ts
+++ tests/lib.global.test.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-require('../lib/global/checkout.js');
+require('../lib/global/checkout.global.js');
 
 describe('lib.global', () => {
     test('exposes Checkout', () => {
--- vite.module.config.ts
+++ vite.module.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
-import { minifyInlineCSS } from './src/lib/utils/vite-plugins';
+import { minifyInlineCSS } from './src/lib/utils/vite';
 
 export default defineConfig({
     build: {
